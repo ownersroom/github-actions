dbb424e29f146a22d2a32bf44ebf7c470a6eb6bb
Christian A. Jacobsen <christian@jenkins.no>
2019-02-07 22:18:22 +0100

    Hopefully fixed `dash` errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-set -eu
+set -e
 
-sh -c "python3 filters/$1.py ${*:2}"
+if [ -z $1 ]; then
+    echo "Please specify an action, e.g. 'file' and it's arguments"
+    exit 1
+fi
+
+sh -c "python3 filters/$1.py ${@:2}"
